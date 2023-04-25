29b417d869d026286b7a2f160bc395b738b4a748
Lario Borges <lario.borges@gmail.com>
2023-04-25 15:05:18 +0200

    redux encrypted persist

--- modules/base-react-ts/src/index.tsx
+++ modules/base-react-ts/src/index.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
-import { store } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from 'redux/store';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,7 +12,12 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={Router} />
+      <PersistGate
+        loading={null}
+        persistor={persistor}
+      >
+        <RouterProvider router={Router} />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
--- modules/base-react-ts/src/types/redux/store.ts
+++ modules/base-react-ts/src/types/redux/store.ts
@@ -1,5 +1,11 @@
 import { AnyAction, ThunkDispatch } from '@reduxjs/toolkit';
 import { store } from 'redux/store';
+import { AppState } from './app';
+import { CounterState } from './counter';
 
 export type AppDispatch = typeof store.dispatch & ThunkDispatch<RootState, null, AnyAction>;
 export type RootState = ReturnType<typeof store.getState>;
+export interface InitialState {
+  app: AppState;
+  counter: CounterState;
+}
