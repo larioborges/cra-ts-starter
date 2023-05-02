b80199f417d39c9422dd52ce6976bc9ab0976500
Lario Borges <lario.borges@gmail.com>
2023-05-02 10:40:44 +0200

    prettier format from config file and accross all modules

--- modules/local-proxy/src/index.js
+++ modules/local-proxy/src/index.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-console.log(process.env.API_PORT)
+console.log(process.env.API_PORT);
 
 const app = express();
 const PORT = 8080;
-const HOST = "localhost";
+const HOST = 'localhost';
 
-app.use('/api', createProxyMiddleware({
+app.use(
+  '/api',
+  createProxyMiddleware({
     target: `http://localhost:${process.env.API_PORT ? process.env.API_PORT : 4000}`,
     changeOrigin: true,
-}));
+  }),
+);
 
-app.use('/', createProxyMiddleware({
+app.use(
+  '/',
+  createProxyMiddleware({
     target: `http://localhost:${process.env.WEB_PORT ? process.env.WEB_PORT : 3000}`,
     changeOrigin: true,
-}));
+  }),
+);
 
 app.listen(PORT, HOST, () => {
-    console.log(`Starting Proxy at ${HOST}:${PORT}`);
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
 });
