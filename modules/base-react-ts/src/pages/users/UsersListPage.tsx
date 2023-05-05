aa41c7a064af58154c2776e53e1adef697540dcd
Lario Borges <lario.borges@gmail.com>
2023-05-05 16:45:08 +0200

    Code cleanup

--- modules/base-react-ts/src/pages/users/UsersListPage.tsx
+++ modules/base-react-ts/src/pages/users/UsersListPage.tsx
@@ -5,17 +5,15 @@ import React from 'react';
 import { useGetUsersQuery } from 'redux/api';
 
 const UsersListPage: React.FC<{}> = (): JSX.Element => {
-  // console.log(userMap);
   const { data: users, error, isError, isLoading } = useGetUsersQuery();
-
-  console.log('users', users);
-  console.log('error', error);
-  console.log('isLoading', isLoading);
   return (
     <React.Fragment>
       <h1>Users List</h1>
       <Loader isLoading={isLoading} />
-      <ErrorMsg isError={isError} />
+      <ErrorMsg
+        isError={isError}
+        errorMsg={(error as any)?.data?.errorMsg}
+      />
       <UserList users={users} />
     </React.Fragment>
   );
